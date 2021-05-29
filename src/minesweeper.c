d381e31b017369b1d31d3dd543dcdddb899a26fe
Conor Smith <conormartindale@gmail.com>
2021-05-29 11:54:23 +1000

    Board creation is functional

--- src/minesweeper.c
+++ src/minesweeper.c
@@ -10,13 +10,52 @@ typedef struct MineSweeperGame {
     bool gameStarted;
 } Game;
 
-//Documentation for public functions in header file
+//These are 'private' functions. They are for internal use and are not declared in the header file
+
+/* Returns double pointer of tiles
+ * Creates an array of size length * height, then assigns it to a pointer
+ * Assigns subsequent pointers to multiples of length, creating a 2D array*/
+enum Tile** createBoard(int length, int height) {
+    enum Tile** board = malloc(length * sizeof(enum Tile*));
+    enum Tile* tileArray = calloc(length * height, sizeof(enum Tile));
+
+    for(int i = 0;i < length;i++) {
+        board[i] = tileArray + length * i;
+    }
+
+    return board;
+}
+
+// Frees the board specificaly
+void freeBoard(enum Tile** board) {
+    free(board[0]);
+    free(board);
+}
+
+//All following functions are declared in header file
 
 Game* createGame(int length, int height, int mines) {
-    return malloc(sizeof(Game));
+    length = length >= 1 ? length : 1;
+    height = height >= 1 ? height : 1;
+    mines = mines >= 0 ? mines : 0;
+    mines = mines <= length * height ? mines : length * height;
+
+    Game* game = malloc(sizeof(Game));
+    game->length = length;
+    game->height = height;
+    game->mines = mines;
+    game->flagged = 0;
+    game->revealed = 0;
+    game->gameState = ACTIVE;
+    game->gameStarted = false;
+    game->board = createBoard(length, height);
+
+    return game;
 }
 
 void deleteGame(Game* game) {
+    freeBoard(game->board);
+    free(game);
 }
 
 void resetGame(int length, int height, int mines) {
